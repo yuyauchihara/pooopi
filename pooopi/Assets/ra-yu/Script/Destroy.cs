3c3fea33fd75ff44243e98539d36b04c9fca26c2
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2023-01-26 11:45:55 +0900

    うんち

---
+++ pooopi/Assets/ra-yu/Script/Destroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //接触したオブジェクトのタグが"Player"のとき
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
